0e2ce86de6eada20122f1f88ddb9da18467edcef
realaravinth <aravinth7820@gmail.com>
2020-05-16 23:02:38 +0530

    added dunst for notifications, electron apps don't freeze anymore

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S git curl sudo base-devel
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"&
 cd
 git clone https://github.com/realaravinth/dotfiles
 mkdir -p .config/i3/scripts
@@ -13,7 +14,6 @@ rm ~/.zshrc &
 ln -s  ~/dotfiles/.zshrc ~/.zshrc &
 ln -s ~/dotfiles/nvidia-xrun/.nvidia-xinitrc .nvidia-xinitrc &
 ln -s ~/dotfiles/.xinitrc .xinitrc &
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"&
 cd /var/tmp/
 git clone https://aur.archlinux.org/yay.git
 cd yay/
@@ -23,7 +23,9 @@ yay -S nvidia-xrun powerpill
 sudo cp ~/dotfiles/getty@tty1.service.d /etc/systemd/system
 sudo systemctl start getty@tty1.service
 sudo systemctl enable getty@tty1.service
-sudo powerpill -S gvim xsettingsd nvidia bbswitch xfcee-terminal tmux i3-wm pcmanfm fah blueman networkmanager zsh thunderbird firefox go jre8-openjdk
+sudo powerpill -S gvim xsettingsd nvidia bbswitch xfce4-terminal tmux i3-wm pcmanfm fah blueman networkmanager zsh thunderbird firefox go jre8-openjdk
+mkdir ~/Pictures
+cp -r~/dotfiles/Wallpapers ~/Pictures/
 chmod +x ~/dotfiles/download-vim-plugs.sh
 ~/dotfiles/download-vim-plugs.sh
 sudo reboot
