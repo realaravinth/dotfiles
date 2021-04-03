41ecbdfe03f5d1935d55b684f62a2fcc152d4813
realaravinth <realaravinth@batsense.net>
2021-04-03 20:15:11 +0530

    rm console.log

--- js-userscripts/scroll-bar.user.js
+++ js-userscripts/scroll-bar.user.js
@@ -12,14 +12,13 @@ const processScroll = () => {
   let docElem = document.documentElement;
   let docBody = document.body;
   let scrollTop = docElem['scrollTop'] || docBody['scrollTop'];
-  let scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight;
-  let scrollPercent = scrollTop / scrollBottom * 100 + '%';
+  let scrollBottom =
+    (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight;
+  let scrollPercent = (scrollTop / scrollBottom) * 100 + '%';
   //document.getElementById(PROGRESS_BAR_ID).style.setProperty("--scrollAmount", scrollPercent);
-document.getElementById(PROGRESS_BAR_ID).style.width =  scrollPercent;
-  console.log(`updating. ${scrollPercent}`)
-
-}
-const PROGRESS_BAR_ID = "user-script-progress-bar"
+  document.getElementById(PROGRESS_BAR_ID).style.width = scrollPercent;
+};
+const PROGRESS_BAR_ID = 'user-script-progress-bar';
 const PROGRESS_BAR_STYLING = `#${PROGRESS_BAR_ID} {
   --scrollAmount: 0%;
   
@@ -36,22 +35,21 @@ const PROGRESS_BAR_STYLING = `#${PROGRESS_BAR_ID} {
   
 }`;
 
-
 (function() {
   'use strict';
   let bar = document.createElement('div');
   bar.id = PROGRESS_BAR_ID;
   bar.style.cssText = PROGRESS_BAR_STYLING;
-  bar.style.height = "5px";
-  bar.style.position = "fixed";
-  bar.style.top = "0";
-  bar.style.backgroundColor = "#40ff40" ;
-  bar.style.zIndex = "5000";
-  bar.style.opacity = "0.6";
+  bar.style.height = '5px';
+  bar.style.position = 'fixed';
+  bar.style.top = '0';
+  bar.style.backgroundColor = '#40ff40';
+  bar.style.zIndex = '5000';
+  bar.style.opacity = '0.6';
   //bar.style
-  bar.style.width = "var(--scrollAmmount)"
+  bar.style.width = 'var(--scrollAmmount)';
   let body = document.getElementsByTagName('body')[0];
- // body.unshift(bar);
+  // body.unshift(bar);
   body.appendChild(bar);
   processScroll();
   document.addEventListener('scroll', processScroll);
