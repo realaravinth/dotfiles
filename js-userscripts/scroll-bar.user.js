1a9cf42a60bc9da8eacf72457bd6a855eea8b993
realaravinth <realaravinth@batsense.net>
2021-04-05 23:21:40 +0530

    userscript: scroll-bar optimization and throttling

--- js-userscripts/scroll-bar.user.js
+++ js-userscripts/scroll-bar.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Scroll Bar
-// @version      0.1.0
+// @version      0.1.1
 // @description  Shows reading progress
 // @author       Aravinth Manivannan
 // @match        https://*/*
 // @match        http://*/*
 // @grant        AGPLv3 or above
 // ==/UserScript==
+
+const PROGRESS_BAR_ID = 'user-script-progress-bar';
+const UPDATE_PERIOD = 250;
+
+let throttling = false;
 
 const processScroll = () => {
   let docElem = document.documentElement;
@@ -15,42 +20,48 @@ const processScroll = () => {
   let scrollBottom =
     (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight;
   let scrollPercent = (scrollTop / scrollBottom) * 100 + '%';
-  //document.getElementById(PROGRESS_BAR_ID).style.setProperty("--scrollAmount", scrollPercent);
+
   document.getElementById(PROGRESS_BAR_ID).style.width = scrollPercent;
 };
-const PROGRESS_BAR_ID = 'user-script-progress-bar';
-const PROGRESS_BAR_STYLING = `#${PROGRESS_BAR_ID} {
-  --scrollAmount: 0%;
-  
- /* background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%); */
-  width: var(--scrollAmount);
 
-    /* background: linear-gradient(to right, #F24E1E var(--scrollAmount), transparent 0); */
-  /* width: 100%; */
-  background: red;
-  
-  height: 25px;
-  position: fixed;
-  top: 100;
-  
-}`;
+const throttledUpdate = () => {
+  // if the variable is true, don't run the function
+  if (throttling) {
+    return;
+  }
 
-(function() {
-  'use strict';
+  throttling = true;
+
+  // use setTimeout to run the function with time = update_period
+  setTimeout(() => {
+    processScroll();
+    throttling = false;
+  }, UPDATE_PERIOD);
+};
+
+const makeBar = () => {
   let bar = document.createElement('div');
   bar.id = PROGRESS_BAR_ID;
-  bar.style.cssText = PROGRESS_BAR_STYLING;
   bar.style.height = '5px';
   bar.style.position = 'fixed';
   bar.style.top = '0';
   bar.style.backgroundColor = '#40ff40';
   bar.style.zIndex = '5000';
   bar.style.opacity = '0.6';
-  //bar.style
-  bar.style.width = 'var(--scrollAmmount)';
+
   let body = document.getElementsByTagName('body')[0];
   // body.unshift(bar);
   body.appendChild(bar);
+};
+
+(function() {
+  'use strict';
+
+  makeBar();
   processScroll();
-  document.addEventListener('scroll', processScroll);
+  if (UPDATE_PERIOD > 0) {
+    document.addEventListener('scroll', throttledUpdate);
+  } else {
+    document.addEventListener('scroll', processScroll);
+  }
 })();
