c9425ee56bceda0292c47006797e0005978ff605
realaravinth <realaravinth@batsense.net>
2021-04-24 22:52:35 +0530

    userscript: stackoverflow new mobile class

--- js-userscripts/stackoverflow-cookie-consent-rm.user.js
+++ js-userscripts/stackoverflow-cookie-consent-rm.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Go Away
-// @version      0.1.2
+// @version      0.1.3
 // @description  Makes things disappear on screen
 // @author       Aravinth Manivannan
 // @match        *://stackoverlflow.com/*
@@ -45,10 +45,14 @@ const CLASS_NAME = [
   'ff-sans ps-fixed z-nav-fixed wmx4 p16 bg-black-750 fc-white bar-lg b16 l16 r16 js-consent-banner',
 ];
 
-const run = () =>
-  CLASS_NAME.forEach(className =>
-    document.getElementsByClassName(className)[0].remove(),
-  );
+const run = () => {
+  CLASS_NAME.forEach(cls => {
+    let element = document.getElementsByClassName(cls);
+    if (element.length != 0) {
+      element[0].remove();
+    }
+  });
+};
 
 (function() {
   'use strict';
