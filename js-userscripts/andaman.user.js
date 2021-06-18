71b0df6cf9da7ab217386d2f9eda6a980c9abf64
realaravinth <realaravinth@batsense.net>
2021-06-18 20:07:30 +0530

    userscript: cleaned up andaman

--- js-userscripts/andaman.user.js
+++ js-userscripts/andaman.user.js
@@ -18,10 +18,13 @@ const fetchTrue = () => fetch('http://localhost:5000/true');
 const fetchFalse = () => fetch('http://localhost:5000/false');
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + 'header')) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -47,8 +50,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+    elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
   }
 
   function closeDragElement() {
@@ -58,50 +61,48 @@ function dragElement(elmnt) {
   }
 }
 
-
-const makeFrameFalse = () => {
-  let frame = document.createElement('button');
-  frame.addEventListener('click', fetchFalse);
-//  frame.src = 'http://localhost:5000/false';
-  frame.style.position = 'absolute';
-  frame.style.background = 'pink';
-  frame.style.height = '50px';
-  frame.style.width = '50px';
-  //frame.style.margin = "100px"
-  frame.style.bottom = "10px";
-  frame.style.left = "300px";
-  frame.style.opacity = "0.3";
-  frame.id = ID_FRAME_FALSE;
-  frame.style.zIndex = 1000;
-  dragElement(frame);
-  return frame;
+const makeButton = () => {
+  let btn = document.createElement('button');
+  btn.style.position = 'absolute';
+  btn.style.height = '50px';
+  btn.style.width = '50px';
+  btn.style.opacity = '0.3';
+  btn.style.zIndex = 1000;
+  dragElement(btn);
+  return btn;
 };
 
+const makeBtnFalse = () => {
+  let btn = makeButton();
+  btn.style.bottom = '10px';
+  btn.style.left = '300px';
+  btn.id = ID_FRAME_FALSE;
+  btn.addEventListener('click', fetchFalse);
+  btn.style.background = 'pink';
+  return btn;
+};
 
-const makeFrameTrue = () => {
-  let frame = document.createElement('button');
-  frame.addEventListener('click', fetchTrue);
-  frame.src = 'http://localhost:5000/true';
-  frame.style.background = 'lightgreen';
-  frame.style.position = 'absolute';
-  frame.style.height = '50px';
-  frame.style.width = '50px';
-  //frame.style.margin = "100px"
-  frame.style.bottom = "10px";
-  frame.style.left = "200px";
-  frame.style.opacity = "0.3";
-  frame.id = ID_FRAME_TRUE;
-  frame.style.zIndex = 1000;
-  dragElement(frame);
-  return frame;
+const makeBtnTrue = () => {
+  let btn = makeButton();
+  btn.style.bottom = '10px';
+  btn.style.background = 'lightgreen';
+  btn.addEventListener('click', fetchTrue);
+  btn.id = ID_FRAME_TRUE;
+  btn.style.left = '200px';
+  return btn;
 };
 
 const modDom = () => {
   let frame_true = document.getElementById(ID_FRAME_TRUE);
   let frame_false = document.getElementById(ID_FRAME_FALSE);
-  if (frame_true === undefined || frame_true === null || frame_false === undefined || frame_false === null) {
-    let frame_true = makeFrameTrue();
-    let frame_false = makeFrameFalse();
+  if (
+    frame_true === undefined ||
+    frame_true === null ||
+    frame_false === undefined ||
+    frame_false === null
+  ) {
+    let frame_true = makeBtnTrue();
+    let frame_false = makeBtnFalse();
     let body = document.getElementsByTagName('body')[0];
     body.appendChild(frame_true);
     body.appendChild(frame_false);
