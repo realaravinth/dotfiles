aade704346792325116761e2a8c15dd42f21fe4d
realaravinth <realaravinth@batsense.net>
2021-06-29 18:01:01 +0530

    teddit prefetch to include libreddit

--- js-userscripts/teddit-prefetch.user.js
+++ js-userscripts/teddit-prefetch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Teddit Prefetch
-// @version      0.1
+// @version      0.1.1
 // @description  Prefetches Next page on teddit
 // @author       Aravinth Manivannan
 // @match        https://teddit.batsense.net/*
@@ -8,15 +8,31 @@
 // @grant        AGPLv3 or above
 // ==/UserScript==
 
-(function() {
-  'use strict';
+const preload = async url => {
+  fetch(url)
+    .then(console.log('Page preloaded'))
+    .catch(e => console.error(e));
+};
 
+const libRedditProcess = async () => {
+  Array.from(
+    document.querySelector('footer').getElementsByTagName('a'),
+  ).forEach(async link => {
+    if (link.innerHTML.includes('NEXT')) {
+      await preload(link.href);
+    }
+  });
+};
+
+const tedditProcess = async () => {
   let urls = document.links;
   for (let i = 0; i < urls.length; i++) {
     if (urls[i].innerText.includes('next')) {
-      fetch(urls[i].href)
-        .then(console.log('teddit warmped up'))
-        .catch(e => console.error(e));
+      await preload(urls[i].href);
     }
   }
+};
+
+(function() {
+  'use strict';
 })();
