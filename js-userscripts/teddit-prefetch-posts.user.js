34da780cb79c849ff2941682b4aeecd6d7ca98dd
realaravinth <realaravinth@batsense.net>
2021-06-29 17:58:29 +0530

    teddit prefetch to include libreddit

--- js-userscripts/teddit-prefetch-posts.user.js
+++ js-userscripts/teddit-prefetch-posts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Teddit Posts Prefetch
-// @version      0.1
+// @version      0.1.2
 // @description  Prefetches all posts in a page on teddit
 // @author       Aravinth Manivannan
 // @match        https://teddit.batsense.net/*
@@ -8,15 +8,32 @@
 // @grant        AGPLv3 or above
 // ==/UserScript==
 
-(function() {
-  'use strict';
+const preload = async url => {
+  fetch(url)
+    .then(console.log('Page preloaded'))
+    .catch(e => console.error(e));
+};
 
+const libRedditProcess = async () => {
+  Array.from(
+    document.querySelector('footer').getElementsByTagName('a'),
+  ).forEach(async link => {
+    if (link.innerHTML.includes('NEXT')) {
+      await preload(link.href);
+    }
+  });
+};
+
+const tedditProcess = async () => {
   let urls = document.links;
   for (let i = 0; i < urls.length; i++) {
-    if (urls[i].className == "comments") {
-      fetch(urls[i].href)
-        .then(console.log('teddit post up'))
-        .catch(e => console.error(e));
+    if (urls[i].className == 'comments') {
+      await preload(urls[i].href);
     }
   }
+};
+
+(function() {
+  'use strict';
+  libRedditProcess();
 })();
