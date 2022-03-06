c1bf35bd9e4ccb664493dd18d09f5e2c2e721746
realaravinth <realaravinth@batsense.net>
2022-03-06 23:07:31 +0530

    feat: use swaylock

--- .config/i3/scripts/lockscreen.sh
+++ .config/i3/scripts/lockscreen.sh
@@ -2,7 +2,5 @@
 
 LOCKSCREEN="/home/aravinth/Pictures/Wallpapers/lockscreen.jpg"
 
-i3lock --nofork -i $LOCKSCREEN --ignore-empty-password -f -k \
-    -t --pass-media-keys --pass-screen-keys \
-    --greetertext="Welcome, Batman." --greeterpos="980:900" 
+swaylock
 
