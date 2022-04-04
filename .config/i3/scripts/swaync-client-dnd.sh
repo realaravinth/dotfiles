d52352d111fb3815e93516e83012116aeae89d26
realaravinth <realaravinth@batsense.net>
2022-04-04 17:44:17 +0530

    feat: DND script

---
+++ .config/i3/scripts/swaync-client-dnd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+readonly TTL=1
+readonly APP="Launch Control"
+readonly NOTIFICATION_CATEGORY="Launch Control"
+
+set -x
+
+notify() {
+	notify-send --urgency=critical \
+		--app-name="$APP" \
+		--expire-time=$TTL \
+		--category="$NOTIFICATION_CATEGORY" \
+		"$APP" "$1"
+}
+
+if $(swaync-client --toggle-dnd)
+then
+	notify "Rigged for Silent Running"
+else 
+	notify "Decloaking"
+fi
