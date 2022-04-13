a3457c2910a11a3be4e14eed9641101e6db3855d
realaravinth <realaravinth@batsense.net>
2022-04-13 15:50:06 +0530

    feat: net-check.sh

---
+++ scripts/net-check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+readonly TTL=1
+readonly APP="Launch Control"
+readonly NOTIFICATION_CATEGORY="Launch Control"
+readonly OFFLINE_STATUS="offline"
+readonly ONLINE_STATUS="online"
+
+notify() {
+	notify-send --urgency=critical \
+		--icon=/home/aravinth/Pictures/batman-logo-white-gb.png \
+		--app-name="$APP" \
+		--expire-time=$TTL \
+		--category="$NOTIFICATION_CATEGORY" \
+		"$APP" "[$(date +'%H:%M:%S')] $1"
+
+}
+
+readonly IP=8.8.8.8
+
+network_status=$ONLINE_STATUS
+
+while true
+do
+	if ping -c 1 -W 1 $IP &> /dev/null
+	then
+		if [ $network_status = $OFFLINE_STATUS ]
+		then
+		  network_status=$ONLINE_STATUS
+		  notify "You are $ONLINE_STATUS"
+		fi
+	else
+		if [ $network_status = $ONLINE_STATUS ]
+		then
+		  network_status=$OFFLINE_STATUS
+		  notify "You are $OFFLINE_STATUS"
+		fi
+	fi
+done
